a6203ced55735b0cce585f767f383b2c59d93ca1
KiloanPro <83006369+KiloanPro@users.noreply.github.com>
2022-01-16 21:38:07 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const mineflayer = require('mineflayer')
+
+const bot = mineflayer.createBot({
+	host: 'Your Ip',
+	port: Your Port,
+	username: 'Steve', # Nur Username angeben wenn 
+	password: '',      # der Bot Cracked sein soll!
+
+})
+
+
